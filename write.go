801d52fedfd024569a60801ff5d0ae49f6a8026b
gobwas <gobwas@gmail.com>
2017-01-26 00:41:22 +0300

    optimize writer, comments

--- write.go
+++ write.go
@@ -3,6 +3,8 @@ package ws
 import (
 	"encoding/binary"
 	"io"
+	"reflect"
+	"unsafe"
 )
 
 const (
@@ -20,55 +22,51 @@ const (
 )
 
 func WriteHeader(w io.Writer, h Header) error {
-	size := 2
-
-	var lenByte byte
-	switch {
-	case h.Length < 126:
-		lenByte = byte(h.Length)
-		size += 0
-
-	case h.Length <= len16:
-		lenByte = 126
-		size += 2
-
-	case h.Length <= len64:
-		lenByte = 127
-		size += 8
-
-	default:
-		return ErrHeaderLengthUnexpected
-	}
-
-	if h.Masked {
-		lenByte |= bit0
-		size += 4
-	}
-
-	bts := make([]byte, size)
+	// Make slice of bytes with capacity 14 that could hold any header.
+	//
+	// We use unsafe to stick bts to stack and avoid allocations.
+	//
+	// Using stack based slice is safe here, cause golang docs for io.Writer
+	// says that "Implementations must not retain p".
+	// See https://golang.org/pkg/io/#Writer
+	var b [14]byte
+	bp := uintptr(unsafe.Pointer(&b))
+	bh := &reflect.SliceHeader{Data: bp, Len: 14, Cap: 14}
+	bts := *(*[]byte)(unsafe.Pointer(bh))
 
 	if h.Fin {
 		bts[0] |= bit0
 	}
 	bts[0] |= h.Rsv << 4
 	bts[0] |= byte(h.OpCode)
-	bts[1] = lenByte
 
-	maskPos := 2 // after fin, rsv and op code byte and length byte.
+	var n int
 	switch {
-	case lenByte == 126:
+	case h.Length < 126:
+		bts[1] = byte(h.Length)
+		n = 2
+
+	case h.Length <= len16:
+		bts[1] = 126
 		binary.BigEndian.PutUint16(bts[2:], uint16(h.Length))
-		maskPos += 2
-	case lenByte == 127:
+		n = 4
+
+	case h.Length <= len64:
+		bts[1] = 127
 		binary.BigEndian.PutUint64(bts[2:], uint64(h.Length))
-		maskPos += 8
+		n = 10
+
+	default:
+		return ErrHeaderLengthUnexpected
 	}
 
 	if h.Masked {
-		copy(bts[maskPos:], h.Mask[:])
+		bts[1] |= bit0
+		n += copy(bts[n:], h.Mask[:])
 	}
 
-	_, err := w.Write(bts)
+	_, err := w.Write(bts[:n])
+
 	return err
 }
 
