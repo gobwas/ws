ef8daf1e6ddfa633d5a6e5bd1bfa2ac98bc23280
gobwas <gobwas@gmail.com>
2017-07-10 11:40:32 +0300

    wsutil: helper functions initial commit

---
+++ wsutil/handler.go
@@ -0,0 +1,149 @@
+package wsutil
+
+import (
+	"io"
+	"io/ioutil"
+	"strconv"
+
+	"github.com/gobwas/pool/pbytes"
+	"github.com/gobwas/ws"
+)
+
+type FrameHandler func(h ws.Header, r io.Reader) error
+
+type ClosedError struct {
+	code   ws.StatusCode
+	reason string
+}
+
+func (err ClosedError) Error() string {
+	return "ws closed: " + strconv.FormatUint(uint64(err.code), 10) + " " + err.reason
+}
+
+func (err ClosedError) Reason() string {
+	return err.reason
+}
+
+func (err ClosedError) Code() ws.StatusCode {
+	return err.code
+}
+
+func PingHandler(w io.Writer, state ws.State) FrameHandler {
+	return func(h ws.Header, rd io.Reader) (err error) {
+		if h.Length == 0 {
+			// The most common case when ping is empty.
+			return ws.WriteHeader(w, ws.Header{
+				Fin:    true,
+				OpCode: ws.OpPong,
+				Masked: state.Is(ws.StateClientSide),
+			})
+		}
+
+		// In other way reply with Pong frame with copied payload.
+		// Note that int(h.Length) is safe here because control frame i
+		// could be <= 125 bytes length by RFC.
+		p := pbytes.GetBufLen(int(h.Length) + ws.HeaderSize(ws.Header{
+			Length: h.Length,
+			OpCode: ws.OpPong,
+			Masked: state.Is(ws.StateClientSide),
+		}))
+		defer pbytes.PutBuf(p)
+
+		w := NewControlWriterBuffer(w, state, ws.OpPong, p)
+		_, err = io.Copy(w, rd)
+		if err == nil {
+			err = w.Flush()
+		}
+
+		return err
+	}
+}
+
+func PongHandler(w io.Writer, state ws.State) FrameHandler {
+	return func(h ws.Header, rd io.Reader) (err error) {
+		if h.Length == 0 {
+			return nil
+		}
+
+		// int(h.Length) is safe here because control frame could be < 125
+		// bytes length by RFC.
+		buf := pbytes.GetBufLen(int(h.Length))
+		defer pbytes.PutBuf(buf)
+
+		// Discard pong message according to the RFC6455:
+		// A Pong frame MAY be sent unsolicited. This serves as a
+		// unidirectional heartbeat. A response to an unsolicited Pong frame
+		// is not expected.
+		_, err = io.CopyBuffer(ioutil.Discard, rd, buf)
+
+		return
+	}
+}
+
+func CloseHandler(w io.Writer, state ws.State) FrameHandler {
+	return func(h ws.Header, rd io.Reader) (err error) {
+		var (
+			f      ws.Frame
+			code   ws.StatusCode
+			reason string
+		)
+		if h.Length == 0 {
+			f = ws.CloseFrame
+			code = ws.StatusNoStatusRcvd
+		} else {
+			// int(h.Length) is safe here because control frame could be < 125
+			// bytes length by RFC.
+			p := pbytes.GetBufLen(int(h.Length))
+			defer pbytes.PutBuf(p)
+
+			_, err = io.ReadFull(rd, p)
+			if err != nil {
+				return
+			}
+
+			code, reason = ws.ParseCloseFrameData(p)
+
+			if e := ws.CheckCloseFrameData(code, reason); e != nil {
+				f = ws.NewCloseFrame(ws.StatusProtocolError, e.Error())
+			} else {
+				// RFC6455#5.5.1:
+				// If an endpoint receives a Close frame and did not previously
+				// send a Close frame, the endpoint MUST send a Close frame in
+				// response. (When sending a Close frame in response, the endpoint
+				// typically echos the status code it received.)
+				f = ws.NewCloseFrame(code, "")
+			}
+		}
+
+		if state.Is(ws.StateClientSide) {
+			f = ws.MaskFrameInPlace(f)
+		}
+
+		if err = ws.WriteFrame(w, f); err == nil {
+			err = ClosedError{code, reason}
+		}
+
+		return
+	}
+}
+
+// ControlHandler return FrameHandler that handles control messages
+// regarding to the given state and writes responses to w when
+// needed.
+func ControlHandler(w io.Writer, state ws.State) FrameHandler {
+	pingHandler := PingHandler(w, state)
+	pongHandler := PongHandler(w, state)
+	closeHandler := CloseHandler(w, state)
+
+	return func(h ws.Header, rd io.Reader) (err error) {
+		switch h.OpCode {
+		case ws.OpPing:
+			return pingHandler(h, rd)
+		case ws.OpPong:
+			return pongHandler(h, rd)
+		case ws.OpClose:
+			return closeHandler(h, rd)
+		}
+		return
+	}
+}
---
+++ wsutil/helper.go
@@ -0,0 +1,211 @@
+package wsutil
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+
+	"github.com/gobwas/ws"
+)
+
+// Message represents a message from peer, that could be presented in one or
+// more frames. That is, it contains payload of all message fragments and
+// operation code of initial frame for this message.
+type Message struct {
+	OpCode  ws.OpCode
+	Payload []byte
+}
+
+// ReadMessage is a helper function that reads next message from r. It appends
+// received message(s) to the third argument and returns the result of it and
+// an error if some failure happened. That is, it probably could receive more
+// than one message when peer sending fragmented message in multiple frames and
+// want to send some control frame between fragments. Then returned slice will
+// contain those control frames at first, and then result of gluing fragments.
+func ReadMessage(r io.Reader, s ws.State, m []Message) ([]Message, error) {
+	rd := Reader{
+		Source:    r,
+		State:     s,
+		CheckUTF8: true,
+		OnIntermediate: func(hdr ws.Header, src io.Reader) error {
+			bts, err := ioutil.ReadAll(src)
+			if err != nil {
+				return err
+			}
+			m = append(m, Message{hdr.OpCode, bts})
+			return nil
+		},
+	}
+	bts, err := ioutil.ReadAll(&rd)
+	if err != nil {
+		return m, err
+	}
+
+	return append(m, Message{rd.Header().OpCode, bts}), nil
+}
+
+// ReadData is a helper function that reads next data (non-control) message
+// from rw.
+// It takes care on handling all control frames. It will send response on
+// control frame to write part of given rw. It blocks until some data frame
+// will be received.
+func ReadData(rw io.ReadWriter, s ws.State) ([]byte, ws.OpCode, error) {
+	return readData(rw, s, ws.OpText|ws.OpBinary)
+}
+
+// ReadClientData reads next data message from rw, considering that caller
+// represents server side. It is a shortcut for ReadData(rw, ws.StateServerSide).
+func ReadClientData(rw io.ReadWriter) ([]byte, ws.OpCode, error) {
+	return ReadData(rw, ws.StateServerSide)
+}
+
+// ReadClientText reads next text message from rw, considering that caller
+// represents server side. It is a shortcut for ReadData(rw, ws.StateServerSide).
+// It discards recieved binary messages.
+func ReadClientText(rw io.ReadWriter) ([]byte, error) {
+	p, _, err := readData(rw, ws.StateServerSide, ws.OpText)
+	return p, err
+}
+
+// ReadClientBinary reads next binary message from rw, considering that caller
+// represents server side. It is a shortcut for ReadData(rw, ws.StateServerSide).
+// It discards recieved text messages.
+func ReadClientBinary(rw io.ReadWriter) ([]byte, error) {
+	p, _, err := readData(rw, ws.StateServerSide, ws.OpBinary)
+	return p, err
+}
+
+// ReadServerData reads next data message from rw, considering that caller
+// represents client side. It is a shortcut for ReadData(rw, ws.StateClientSide).
+func ReadServerData(rw io.ReadWriter) ([]byte, ws.OpCode, error) {
+	return ReadData(rw, ws.StateClientSide)
+}
+
+// ReadServerText reads next text message from rw, considering that caller
+// represents client side. It is a shortcut for ReadData(rw, ws.StateClientSide).
+// It discards recieved binary messages.
+func ReadServerText(rw io.ReadWriter) ([]byte, error) {
+	p, _, err := readData(rw, ws.StateClientSide, ws.OpText)
+	return p, err
+}
+
+// ReadServerBinary reads next binary message from rw, considering that caller
+// represents client side. It is a shortcut for ReadData(rw, ws.StateClientSide).
+// It discards recieved text messages.
+func ReadServerBinary(rw io.ReadWriter) ([]byte, error) {
+	p, _, err := readData(rw, ws.StateClientSide, ws.OpBinary)
+	return p, err
+}
+
+// ReadClientMessage reads next message from r, considering that caller
+// represents server side.
+func ReadClientMessage(r io.Reader, m []Message) ([]Message, error) {
+	return ReadMessage(r, ws.StateServerSide, m)
+}
+
+// ReadServerMessage reads next message from r, considering that caller
+// represents client side.
+func ReadServerMessage(r io.Reader, m []Message) ([]Message, error) {
+	return ReadMessage(r, ws.StateClientSide, m)
+}
+
+// WriteMessage is a helper function that writes message to the w. It
+// constructs single frame with given operation code and payload.
+// It uses given state to prepare side-dependtend things, like cipher
+// payload bytes from client to server. It will not mutate p bytes if
+// cipher must be made.
+//
+// If you want to write message in fragmented frames, use Writer instead.
+func WriteMessage(w io.Writer, s ws.State, op ws.OpCode, p []byte) error {
+	return writeFrame(w, s, op, true, p)
+}
+
+// WriteServerMessage writes message to w, considering that caller
+// represents server side.
+func WriteServerMessage(w io.Writer, op ws.OpCode, p []byte) error {
+	return WriteMessage(w, ws.StateServerSide, op, p)
+}
+
+// WriteServerText is the same as WriteServerMessage with
+// ws.OpText.
+func WriteServerText(w io.Writer, p []byte) error {
+	return WriteServerMessage(w, ws.OpText, p)
+}
+
+// WriteServerBinary is the same as WriteServerMessage with
+// ws.OpBinary.
+func WriteServerBinary(w io.Writer, p []byte) error {
+	return WriteServerMessage(w, ws.OpBinary, p)
+}
+
+// WriteClientMessage writes message to w, considering that caller
+// represents client side.
+func WriteClientMessage(w io.Writer, op ws.OpCode, p []byte) error {
+	return WriteMessage(w, ws.StateClientSide, op, p)
+}
+
+// WriteClientText is the same as WriteClientMessage with
+// ws.OpText.
+func WriteClientText(w io.Writer, p []byte) error {
+	return WriteClientMessage(w, ws.OpText, p)
+}
+
+// WriteClientBinary is the same as WriteClientMessage with
+// ws.OpBinary.
+func WriteClientBinary(w io.Writer, p []byte) error {
+	return WriteClientMessage(w, ws.OpBinary, p)
+}
+
+// HandleClientControl handles control frame from conn and writes
+// response when needed. It considers that caller represents server
+// side.
+func HandleClientControl(conn io.ReadWriter, op ws.OpCode, p []byte) error {
+	return ControlHandler(conn, ws.StateServerSide)(ws.Header{
+		Length: int64(len(p)),
+		OpCode: op,
+	}, bytes.NewReader(p))
+}
+
+// HandleServerControl handles control frame from conn and writes
+// response when needed. It considers that caller represents client
+// side.
+func HandleServerControl(conn io.ReadWriter, op ws.OpCode, p []byte) error {
+	return ControlHandler(conn, ws.StateClientSide)(ws.Header{
+		Length: int64(len(p)),
+		OpCode: op,
+	}, bytes.NewReader(p))
+}
+
+func readData(rw io.ReadWriter, s ws.State, want ws.OpCode) ([]byte, ws.OpCode, error) {
+	ch := ControlHandler(rw, s)
+
+	rd := Reader{
+		Source:         rw,
+		State:          s,
+		CheckUTF8:      true,
+		OnIntermediate: ch,
+	}
+
+	for {
+		hdr, err := rd.NextFrame()
+		if err != nil {
+			return nil, 0, err
+		}
+		if hdr.OpCode.IsControl() {
+			if err := ch(hdr, &rd); err != nil {
+				return nil, 0, err
+			}
+			continue
+		}
+		if hdr.OpCode&want == 0 {
+			if err := rd.Discard(); err != nil {
+				return nil, 0, err
+			}
+			continue
+		}
+
+		bts, err := ioutil.ReadAll(&rd)
+
+		return bts, hdr.OpCode, err
+	}
+}
