4be3cf8a49d2a8b313e957c063ee0e6dc77ff84f
Oleg Kovalov <oleg@hey.com>
2023-04-09 20:18:13 +0200

    ws: adopt purego

--- util.go
+++ util.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"reflect"
-	"unsafe"
 
 	"github.com/gobwas/httphead"
 )
@@ -39,19 +37,6 @@ func SelectEqual(v string) func(string) bool {
 	return func(p string) bool {
 		return v == p
 	}
-}
-
-func strToBytes(str string) (bts []byte) {
-	s := (*reflect.StringHeader)(unsafe.Pointer(&str))
-	b := (*reflect.SliceHeader)(unsafe.Pointer(&bts))
-	b.Data = s.Data
-	b.Len = s.Len
-	b.Cap = s.Len
-	return bts
-}
-
-func btsToString(bts []byte) (str string) {
-	return *(*string)(unsafe.Pointer(&bts))
 }
 
 // asciiToInt converts bytes to int.
